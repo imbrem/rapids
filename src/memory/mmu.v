cdfc0f04a9258e0da7f83cbd6b319f4af11da21f
Jad Ghalayini <jad.ghalayini@hotmail.com>
2019-04-02 16:14:54 -0400

    Began adding VGA spline

--- src/memory/mmu.v
+++ src/memory/mmu.v
@@ -17,11 +17,22 @@ module MMU(
   output wait_instr, // High if the instruction needs time to load
   output wait_data, // High if the data needs time to load
   output instr_segv, // High if invalid instruction address
-  output data_segv // High if invalid data address
+  output data_segv, // High if invalid data address
+  output[8:0] vga_x,
+  output[7:0] vga_y,
+  output[23:0] vga_color,
+  output vga_plot
   );
+
+  localparam VGA_POS = 15'h1;
 
   reg[31:0] special[14:0];
   reg[31:0] memory[127:0];
+
+  wire data_rd_segv;
+  wire data_wd_segv;
+
+  assign data_segv = (rd & data_rd_segv) | (wd & data_wd_segv);
 
   wire[6:0] t_instr_addr = (instr_addr - 16);
   wire[6:0] t_data_addr = (data_addr - 16);
@@ -33,8 +44,18 @@ module MMU(
   wire instr_too_high = t_instr_addr >= 128;
   wire data_too_high = t_data_addr >= 128;
 
+  // Plot to the VGA if and only if we write to the appropriate range
+  assign vga_plot = (data_addr[31:17] == VGA_POS) & wd;
+  // X, Y on the VGA are just the first 17 bits of the address
+  assign vga_y = data_addr[16:9];
+  assign vga_x = data_addr[8:0];
+  // Color is just the data being written, or rather the first 24 bits of it
+  assign vga_color = data_in[23:0];
+
   assign instr_segv = instr_too_high | instr_special;
-  assign data_segv = (data_addr == 0) | data_too_high;
+  assign data_rd_segv = (data_addr == 0) | data_too_high;
+  assign data_wd_segv = (data_addr == 0)
+    | ((data_addr[31:17] != VGA_POS) & data_too_high);
 
   assign wait_data = 1'b0;
   assign wait_instr = 1'b0;
